6533752e29e7dcc8ee6d4e8b92683993ecf1b807
Pea M. Tyczynska <CrystalPea@github.com>
2016-11-22 13:17:48 +0000

    Shotgun added to our setup

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 
+set :session_secret, 'himitsu'
+
 get '/' do
-  "Hello World"
+  "hello!"
+end
+
+get '/secret' do
+  "what is it?"
+end
+
+get '/flower' do
+  "rose is a rose is a rose is a rose"
+end
+
+get '/fish' do
+  "swims in the ocean"
 end
